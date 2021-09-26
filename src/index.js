cdd05f1cb7e6c59fce94c7f1b7eb787edfcd6c74
ext.lishanshan6 <ext.lishanshan6@jd.com>
2021-09-26 11:01:29 +0800

    规范一级目录

---
+++ src/index.js
@@ -0,0 +1,9 @@
+/*
+ * @Author: 李闪闪
+ * @Date: 2021-09-26 10:57:30
+ * @Email: lishanshan6@jd.com
+ * @LastEditors: 李闪闪
+ * @LastEditTime: 2021-09-26 10:57:53
+ * @Description: 
+ */
+alert('fast-cache')
