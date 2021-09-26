c8e20fb1dc4b6f24746412fdd3cb06d68ec85ba4
ext.lishanshan6 <ext.lishanshan6@jd.com>
2021-09-26 14:02:42 +0800

    ## 写代码

--- src/index.js
+++ src/index.js
@@ -3,7 +3,18 @@
  * @Date: 2021-09-26 10:57:30
  * @Email: lishanshan6@jd.com
  * @LastEditors: 李闪闪
- * @LastEditTime: 2021-09-26 10:57:53
+ * @LastEditTime: 2021-09-26 13:55:50
  * @Description: 
  */
-alert('fast-cache')
+class FastCache {
+    constructor() {
+        this.list = {}
+    }
+    set(key, value) {
+        this.list[key] = value;
+    }
+    get(key) {
+        return this.list[key];
+    }
+}
+window.FastCache = FastCache;
